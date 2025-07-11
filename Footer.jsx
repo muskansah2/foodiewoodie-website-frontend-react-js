296327a3d1c9e82cbe4861a764730751a9f42a32
muskansah2 <149327680+muskansah2@users.noreply.github.com>
2025-07-11 19:36:40 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+  const navigate = useNavigate();
+  return (
+    <footer id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo" />
+          <p>
+            Discover a world of flavor at your fingertips. From street food to fine dining, Foodie Woodie lets you browse, customize, and order your favorite meals with ease. Fast, fresh, and convenient—delicious food is just a click away.
+          </p>
+          <div className="footer-social-icons">
+            <a href="https://www.facebook.com">
+              <img src={assets.facebook_icon} alt="facebook" />
+            </a>
+            <a href="https://www.twitter.com">
+              <img src={assets.twitter_icon} alt="twitter" />
+            </a>
+            <a href="https://www.linkedin.com">
+              <img src={assets.linkedin_icon} alt="linkedin" />
+            </a>
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li onClick={() => navigate("/")}>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+91-1234567895</li>
+            <li>contact@foodiewoodie.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+     
+    </footer>
+  );
+};
+
+export default Footer;
